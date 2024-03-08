eaa389bc146178bc313c256f42825a05870f3d08
EddieCodeHub <eddiemeekin@gmail.com>
2024-03-08 10:45:28 +0000

    docs: wrote code for reduce() methods examples

--- reduce.js
+++ reduce.js
@@ -6,6 +6,9 @@
 
 
 // Summing an array of numbers:
+const nums = [0, 1, 2, 3, 4];
+let sum = nums.reduce((acc, curr) => acc =+ curr, 0); // acc = accumulator, curr = current value, 0 = initial value
+console.log(sum); // 10
 
 
 const teamMembers = [
@@ -32,6 +35,19 @@ const teamMembers = [
 ];
 
 // Totaling a specific object property
+let totalExperience = teamMembers.reduce((acc, curr) => acc += curr.yrsExperience, 0); // not spcifying the initial value will break the code as an object will be passed as the initial value
+console.log(totalExperience); // 16
 
 
 // Grouping by a property, and totaling it too
+let experienceByProffesion = teamMembers.reduce((acc, curr) => {
+  let key = curr.profession;
+  if (!acc[key]) {
+    acc[key] = curr.yrsExperience;
+  } else {
+    acc[key] += curr.yrsExperience;
+  }
+  return acc;
+}, {}); // final {} is setting initial value as an empty object
+
+console.log(experienceByProffesion); // { Developer: 12, Designer: 4 }
